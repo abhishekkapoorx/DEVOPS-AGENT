e2874cbda2e7c53b5bacd39e2448435909aed40d
Abhishek Kapoor <kapoorkk1980@gmail.com>
2025-08-28 14:41:53 +0530

    Add pretty print utilities and update public API
    Introduced new functions for pretty printing messages in the utils module. Updated the __init__.py file to include these new functions in the public API. The pretty_print.py file contains the logic for formatting and displaying messages, enhancing the usability of the messaging system.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,5 @@
 from .loadenv import _set_if_undefined
 
-__all__ = ["_set_if_undefined"]
+from .pretty_print import pretty_print_message, pretty_print_messages
+
+__all__ = ["_set_if_undefined", "pretty_print_message", "pretty_print_messages"]
