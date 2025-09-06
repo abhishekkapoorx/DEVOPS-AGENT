9ca11b4719ae8e6d4db98d996a9fc99f80489ceb
RaghavOG <04raghavsingla28@gmail.com>
2025-09-06 12:34:58 +0530

    Ollama model

--- llms/__init__.py
+++ llms/__init__.py
@@ -1,5 +1,9 @@
 from .groq_models import groq_models
 from .openai_models import openai_models
 from .gemini_models import gemini_models
+from .ollama_models import ollama_models
 
-__all__ = ["groq_models", "openai_models", "gemini_models"]
+DEFAULT_MODEL = groq_models["meta-llama/llama-4-scout-17b-16e-instruct"]
+# DEFAULT_MODEL = ollama_models["gpt-oss:20b"]
+
+__all__ = ["groq_models", "openai_models", "gemini_models", "ollama_models", "DEFAULT_MODEL"]
